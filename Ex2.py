d5a4ff831f89558adc95759a4eaffeea72aa422b
vansoncc <43052978+vansoncc@users.noreply.github.com>
2019-02-25 15:00:33 -0500

    Add files via upload

---
+++ Ex2.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import pytest
+import subprocess
+import os
+import queue
+import threading
+
+
+def con_video():
+    video_list = []
+    files = os.listdir('./')
+    for f in files:
+        if f == 'test.mp4':
+
+            # print("Processing", f)
+
+            video_list.append(f)
+
+    # 720p at 2Mbps and 30fps
+    # 480p at 1Mbps and 30fps
+
+    subprocess.run('ffmpeg -i %s -s hd720 -r 30 -b:v 2M %s'
+                   % (video_list[0], (video_list[0])[:-4] + '720.mp4'),
+                   shell=True)
+    print ('Processed ' + video_list[0] + 'to hd720')
+    subprocess.run('ffmpeg -i %s -s hd480 -r 30 -b:v 1M %s'
+                   % (video_list[0], (video_list[0])[:-4] + '480.mp4'),
+                   shell=True)
+    print ('Processed ' + video_list[0] + 'to hd480')
+
+
+def main():
+    threads = []
+    q = queue.Queue()
+    try:
+        for i in range(1):
+            q.put(i)
+            t = threading.Thread(target=con_video())
+            threads.append(t)
+
+
+    except Exception as error:
+        print(error)
+
+
+    for thread in threads:
+        t.start()
+        thread.join()
+
+
+if __name__ == '__main__':
+    main()
